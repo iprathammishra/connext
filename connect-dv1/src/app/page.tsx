8a538eca622914f788d87604796b542d529f0457
Pratham <mishrapratham.codeit@gmail.com>
2024-02-05 00:33:16 +0530

    Delete connect-dv1 directory

--- connect-dv1/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export const metadata = {
-  title: "Connect, book, attend, review, and enjoy. Hence, stay updated.",
-};
-
-export default function Home() {
-  return <p className=" m-5">Everything seems working.</p>;
-}
